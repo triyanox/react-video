7bc819a49cc1edf4426e88cd395f4b0f981ec4c7
Mohamed Achaq <motriyanox@gmail.com>
2023-12-19 16:39:23 +0100

    chore: add `@vercel/analytics`

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: {
@@ -98,6 +99,7 @@ export default function RootLayout({
               </Link>
             </footer>
           </div>
+          <Analytics />
         </Providers>
       </body>
     </html>
