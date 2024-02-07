d94f6373ecccf9d355c7b0174691e08bb4e69f33
Mohamed Achaq <motriyanox@gmail.com>
2024-02-07 16:11:34 +0100

    fix: fix aspect ration on the root wrapper

--- packages/core/src/ui/video.tsx
+++ packages/core/src/ui/video.tsx
@@ -3,15 +3,15 @@
  *  So, I have to use `@ts-ignore` to ignore the errors.
  * I will remove it once the definitions are up to date.
  */
-import { cn } from "@/lib/utils";
-import * as RadixSlider from "@radix-ui/react-slider";
-import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useRef } from "react";
-import { formatTime } from "../lib/format-time";
-import useVideo from "../lib/hooks/use-video";
-import { VideoProps } from "../types/video.types";
-import Button from "./button";
-import { If, IfElse } from "./helpers";
+import { cn } from '@/lib/utils';
+import * as RadixSlider from '@radix-ui/react-slider';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect, useRef } from 'react';
+import { formatTime } from '../lib/format-time';
+import useVideo from '../lib/hooks/use-video';
+import { VideoProps } from '../types/video.types';
+import Button from './button';
+import { If, IfElse } from './helpers';
 import {
   BackBy10,
   EnterPip,
@@ -23,7 +23,7 @@ import {
   Pause,
   Play,
   Volume,
-} from "./icons";
+} from './icons';
 
 /**
  * This a react video component built on top of `HTMLVideoElement`. It's highly customizable using `tailwindcss` and easy to use.
@@ -72,12 +72,12 @@ function Video({
     }
     if (Array.isArray(src)) {
       src.forEach((s) => {
-        const source = document.createElement("source");
+        const source = document.createElement('source');
         source.src = s.src;
         source.type = s.type;
         videoRef.current?.appendChild(source);
       });
-    } else if (typeof src === "string") {
+    } else if (typeof src === 'string') {
       videoRef.current.src = src;
     } else {
       return;
@@ -141,12 +141,7 @@ function Video({
   return (
     <div
       ref={wrapperRef}
-      className={cn(
-        "rv-relative rv-aspect-video",
-        isLoaded ? "rv-block" : "rv-hidden",
-        className,
-        classNames?.base
-      )}
+      className={cn('relative aspect-video', className, classNames?.base)}
     >
       <video
         crossOrigin="anonymous"
@@ -156,20 +151,20 @@ function Video({
         loop={loop}
         className={cn(
           {
-            "rv-rounded-xl": !isFullscreen,
+            'rounded-xl': !isFullscreen,
           },
           {
-            "rv-h-full rv-w-full": FullScreen,
+            'h-full w-full': FullScreen,
           },
-          classNames?.video
+          classNames?.video,
         )}
         poster={poster}
         preload="metadata"
         autoPlay={autoPlay}
         style={{
-          objectFit: "cover",
-          objectPosition: "center",
-          pointerEvents: "none",
+          objectFit: 'cover',
+          objectPosition: 'center',
+          pointerEvents: 'none',
         }}
       ></video>
       {/* @ts-ignore */}
@@ -184,12 +179,12 @@ function Video({
           exit={{ opacity: 0 }}
           transition={{
             duration: 0.3,
-            ease: "easeInOut",
+            ease: 'easeInOut',
           }}
           className={cn(
-            "rv-absolute rv-inset-0 rv-bg-gradient-to-t rv-from-black rv-via-transparent rv-to-black",
-            { "rv-rounded-xl": !isFullscreen },
-            classNames?.backdrop
+            'rv-absolute rv-inset-0 rv-bg-gradient-to-t rv-from-black rv-via-transparent rv-to-black',
+            { 'rv-rounded-xl': !isFullscreen },
+            classNames?.backdrop,
           )}
         />
       </AnimatePresence>
@@ -204,11 +199,11 @@ function Video({
             exit={{ opacity: 0 }}
             transition={{
               duration: 0.4,
-              ease: "easeInOut",
+              ease: 'easeInOut',
             }}
             className={cn(
-              "rv-absolute rv-inset-0 rv-flex rv-justify-between rv-px-4 rv-h-fit xl:rv-px-8 rv-gap-2 xl:rv-gap-2 rv-py-3 rv-top-0",
-              classNames?.topWrapper
+              'rv-absolute rv-inset-0 rv-flex rv-justify-between rv-px-4 rv-h-fit xl:rv-px-8 rv-gap-2 xl:rv-gap-2 rv-py-3 rv-top-0',
+              classNames?.topWrapper,
             )}
           >
             <div className="rv-w-full rv-flex">
@@ -269,8 +264,8 @@ function Video({
               </Button>
               <RadixSlider.Root
                 className={cn(
-                  "rv-relative rv-z-50 rv-items-center rv-select-none rv-touch-none rv-w-full rv-h-5 rv-hidden lg:rv-flex lg:rv-w-1/4",
-                  classNames?.volumeSlider?.root
+                  'rv-relative rv-z-50 rv-items-center rv-select-none rv-touch-none rv-w-full rv-h-5 rv-hidden lg:rv-flex lg:rv-w-1/4',
+                  classNames?.volumeSlider?.root,
                 )}
                 onValueChange={(value) => {
                   handleVolumeChange(Number(value));
@@ -282,22 +277,22 @@ function Video({
               >
                 <RadixSlider.Track
                   className={cn(
-                    "rv-bg-white rv-backdrop-filter rv-overflow-hidden rv-backdrop-blur-sm rv-h-3 rv-bg-opacity-20 rv-relative rv-grow rv-rounded-full",
-                    classNames?.volumeSlider?.track
+                    'rv-bg-white rv-backdrop-filter rv-overflow-hidden rv-backdrop-blur-sm rv-h-3 rv-bg-opacity-20 rv-relative rv-grow rv-rounded-full',
+                    classNames?.volumeSlider?.track,
                   )}
                 >
                   <RadixSlider.Range
                     className={cn(
-                      "rv-absolute rv-bg-white rv-rounded-full rv-h-full",
-                      classNames?.volumeSlider?.range
+                      'rv-absolute rv-bg-white rv-rounded-full rv-h-full',
+                      classNames?.volumeSlider?.range,
                     )}
                   />
                 </RadixSlider.Track>
                 <If condition={!hideSliderThumb}>
                   <RadixSlider.Thumb
                     className={cn(
-                      "rv-block rv-w-5 rv-h-5 rv-bg-white rv-shadow-[0_2px_2px] rv-shadow-black rv-rounded-[10px] hover:rv-bg-white focus:rv-outline-none focus:rv-shadow-[0_0_0_2px] focus:rv-shadow-white/10",
-                      classNames?.volumeSlider?.thumb
+                      'rv-block rv-w-5 rv-h-5 rv-bg-white rv-shadow-[0_2px_2px] rv-shadow-black rv-rounded-[10px] hover:rv-bg-white focus:rv-outline-none focus:rv-shadow-[0_0_0_2px] focus:rv-shadow-white/10',
+                      classNames?.volumeSlider?.thumb,
                     )}
                     aria-label="Volume"
                   />
@@ -318,14 +313,14 @@ function Video({
             exit={{ opacity: 0 }}
             transition={{
               duration: 0.4,
-              ease: "easeInOut",
+              ease: 'easeInOut',
             }}
             className={cn(
-              "rv-absolute rv-inset-0 xl:rv-mb-0 rv-px-8 rv-gap-8 rv-flex rv-justify-center rv-w-full rv-items-center",
+              'rv-absolute rv-inset-0 xl:rv-mb-0 rv-px-8 rv-gap-8 rv-flex rv-justify-center rv-w-full rv-items-center',
               classNames?.centerWrapper,
               {
-                "rv-mb-8": Boolean(title || subtitle),
-              }
+                'rv-mb-8': Boolean(title || subtitle),
+              },
             )}
           >
             <IfElse
@@ -377,11 +372,11 @@ function Video({
             exit={{ opacity: 0 }}
             transition={{
               duration: 0.4,
-              ease: "easeInOut",
+              ease: 'easeInOut',
             }}
             className={cn(
-              "rv-absolute rv-inset-0 rv-flex rv-flex-col rv-justify-end rv-px-6 xl:rv-px-8 rv-gap-2 lg:rv-gap-3 rv-py-4 xl:rv-py-6 rv-bottom-0",
-              classNames?.bottomWrapper
+              'rv-absolute rv-inset-0 rv-flex rv-flex-col rv-justify-end rv-px-6 xl:rv-px-8 rv-gap-2 lg:rv-gap-3 rv-py-4 xl:rv-py-6 rv-bottom-0',
+              classNames?.bottomWrapper,
             )}
           >
             <div className="rv-w-full rv-flex rv-justify-between rv-items-center rv-gap-21">
@@ -390,21 +385,21 @@ function Video({
                   <If condition={Boolean(subtitle)}>
                     <p
                       className={cn(
-                        "rv-text-[0.5rem] xl:rv-text-base rv-my-0 rv-text-white/80",
-                        classNames?.subtitle
+                        'rv-text-[0.5rem] xl:rv-text-base rv-my-0 rv-text-white/80',
+                        classNames?.subtitle,
                       )}
                     >
-                      {subtitle ?? ""}
+                      {subtitle ?? ''}
                     </p>
                   </If>
                   <If condition={Boolean(title)}>
                     <h2
                       className={cn(
-                        "rv-text-[1rem] xl:rv-text-2xl rv-my-0 rv-font-semibold rv-text-white",
-                        classNames?.title
+                        'rv-text-[1rem] xl:rv-text-2xl rv-my-0 rv-font-semibold rv-text-white',
+                        classNames?.title,
                       )}
                     >
-                      {title ?? ""}
+                      {title ?? ''}
                     </h2>
                   </If>
                 </div>
@@ -412,8 +407,8 @@ function Video({
             </div>
             <RadixSlider.Root
               className={cn(
-                "rv-relative rv-flex rv-items-center rv-select-nones rv-touch-none rv-w-full",
-                classNames?.playbackRateSlider?.root
+                'rv-relative rv-flex rv-items-center rv-select-nones rv-touch-none rv-w-full',
+                classNames?.playbackRateSlider?.root,
               )}
               onValueChange={(value) => {
                 handleSeek(Number(value));
@@ -425,23 +420,23 @@ function Video({
             >
               <RadixSlider.Track
                 className={cn(
-                  "rv-bg-white rv-backdrop-filter rv-overflow-hidden backdrop-blur-sm rv-h-2 lg:rv-h-3 rv-bg-opacity-20 rv-relative rv-grow rv-rounded-full",
-                  classNames?.playbackRateSlider?.track
+                  'rv-bg-white rv-backdrop-filter rv-overflow-hidden backdrop-blur-sm rv-h-2 lg:rv-h-3 rv-bg-opacity-20 rv-relative rv-grow rv-rounded-full',
+                  classNames?.playbackRateSlider?.track,
                 )}
               >
                 <RadixSlider.Range
                   className={cn(
-                    "rv-absolute rv-bg-white rv-h-full",
-                    hideSliderThumb ? "rv-rounded-full" : "rv-rounded-none",
-                    classNames?.playbackRateSlider?.range
+                    'rv-absolute rv-bg-white rv-h-full',
+                    hideSliderThumb ? 'rv-rounded-full' : 'rv-rounded-none',
+                    classNames?.playbackRateSlider?.range,
                   )}
                 />
               </RadixSlider.Track>
               <If condition={!hideSliderThumb}>
                 <RadixSlider.Thumb
                   className={cn(
-                    "rv-block rv-w-3 rv-h-3 lg:rv-w-6 lg:rv-h-6 rv-bg-white rv-rounded-full hover:rv-bg-white focus:rv-outline-none focus:rv-shadow-[0_0_0_2px] focus:rv-shadow-white",
-                    classNames?.playbackRateSlider?.thumb
+                    'rv-block rv-w-3 rv-h-3 lg:rv-w-6 lg:rv-h-6 rv-bg-white rv-rounded-full hover:rv-bg-white focus:rv-outline-none focus:rv-shadow-[0_0_0_2px] focus:rv-shadow-white',
+                    classNames?.playbackRateSlider?.thumb,
                   )}
                   aria-label="Volume"
                 />
