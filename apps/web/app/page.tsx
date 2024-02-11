ba2d41c6f453946bca551efca506fccdf05b7398
Mohamed Achaq <motriyanox@gmail.com>
2024-02-11 15:19:49 +0100

    chore: update the website

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,25 +1,24 @@
-"use client";
-import { GithubIcon } from "@/components/icons";
-import { subtitle, title } from "@/components/primitives";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@nextui-org/button";
-import { Input } from "@nextui-org/input";
-import { Select, SelectItem } from "@nextui-org/select";
-import { Snippet } from "@nextui-org/snippet";
-import { Spinner } from "@nextui-org/spinner";
-import { Switch } from "@nextui-org/switch";
-import { Video } from "@triyanox/react-video";
-import { AnimatePresence, motion } from "framer-motion";
-import Link from "next/link";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+'use client';
+import { GithubIcon } from '@/components/icons';
+import { subtitle, title } from '@/components/primitives';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@nextui-org/button';
+import { Input } from '@nextui-org/input';
+import { Select, SelectItem } from '@nextui-org/select';
+import { Snippet } from '@nextui-org/snippet';
+import { Switch } from '@nextui-org/switch';
+import { Video } from '@triyanox/react-video';
+import { AnimatePresence, motion } from 'framer-motion';
+import Link from 'next/link';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 const packages = [
-  { label: "pnpm", value: "pnpm add @triyanox/react-video" },
-  { label: "npm", value: "npm add @triyanox/react-video" },
-  { label: "yarn", value: "yarn add @triyanox/react-video" },
-  { label: "bun", value: "bun add @triyanox/react-video" },
+  { label: 'pnpm', value: 'pnpm add @triyanox/react-video' },
+  { label: 'npm', value: 'npm add @triyanox/react-video' },
+  { label: 'yarn', value: 'yarn add @triyanox/react-video' },
+  { label: 'bun', value: 'bun add @triyanox/react-video' },
 ];
 
 const schema = z.object({
@@ -36,7 +35,7 @@ const schema = z.object({
 
 export default function Home() {
   const [value, setValue] = useState<Selection>(
-    new Set(["pnpm add @triyanox/react-video"]) as unknown as Selection
+    new Set(['pnpm add @triyanox/react-video']) as unknown as Selection,
   );
   const [loading, setLoading] = useState(true);
 
@@ -45,11 +44,11 @@ export default function Home() {
     defaultValues: {
       autoPlay: true,
       loop: false,
-      title: "Example Video",
-      subtitle: "This is an example video",
+      title: 'Example Video',
+      subtitle: 'This is an example video',
       hideSliderThumb: true,
       multipleSources: false,
-      src: "https://vjs.zencdn.net/v/oceans.mp4",
+      src: 'https://vjs.zencdn.net/v/oceans.mp4',
       showControls: true,
     },
   });
@@ -58,7 +57,7 @@ export default function Home() {
     <section className="flex flex-col items-center justify-center gap-4 py-8">
       <div className="flex w-full justify-center items-center flex-col gap-4 px-4">
         <h3 className={title()}>@triyanox/react-video</h3>
-        <p className={subtitle({ class: "text-center" })}>
+        <p className={subtitle({ class: 'text-center' })}>
           A highly customizable react video component for your app
         </p>
         <div className="flex justify-center items-center w-full gap-2">
@@ -84,108 +83,107 @@ export default function Home() {
         </div>
         <div className="w-full py-16 flex flex-col gap-8 justify-center items-center">
           <div className="flex flex-col gap-4 justify-center items-center w-full px-4">
-            <h3 className={title({ size: "sm" })}>Try it for yourself</h3>
-            <p className={subtitle({ class: "text-center" })}>
+            <h3 className={title({ size: 'sm' })}>Try it for yourself</h3>
+            <p className={subtitle({ class: 'text-center' })}>
               Try changing the values below to see the video component in action
             </p>
           </div>
           <AnimatePresence mode="wait">
             <motion.div
               key={JSON.stringify({
-                src: form.watch("src"),
-                poster: form.watch("poster"),
-                hideSliderThumb: form.watch("hideSliderThumb"),
-                autoPlay: form.watch("autoPlay"),
-                loop: form.watch("loop"),
-                show: form.watch("showControls"),
+                src: form.watch('src'),
+                poster: form.watch('poster'),
+                hideSliderThumb: form.watch('hideSliderThumb'),
+                autoPlay: form.watch('autoPlay'),
+                loop: form.watch('loop'),
+                show: form.watch('showControls'),
               })}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className="w-full min-h-[120px] lg:min-h-[480px] flex justify-center items-center"
+              className="w-full flex justify-center items-center"
             >
               <Video
-                src={form.watch("src")}
-                poster={form.watch("poster")}
-                title={form.watch("title")}
-                subtitle={form.watch("subtitle")}
-                hideSliderThumb={form.watch("hideSliderThumb")}
-                autoPlay={form.watch("autoPlay")}
-                loop={form.watch("loop")}
-                showControls={form.watch("showControls")}
+                src={form.watch('src')}
+                poster={form.watch('poster')}
+                title={form.watch('title')}
+                subtitle={form.watch('subtitle')}
+                hideSliderThumb={form.watch('hideSliderThumb')}
+                autoPlay={form.watch('autoPlay')}
+                loop={form.watch('loop')}
+                showControls={form.watch('showControls')}
                 onLoad={() => {
                   setLoading(false);
                 }}
               />
-              {loading && <Spinner color="default" />}
             </motion.div>
           </AnimatePresence>
           <div className="grid grid-cols-2 lg:grid-cols-4 w-full gap-6">
             <Input
               labelPlacement="outside"
-              value={form.watch("src")}
-              onChange={(e) => form.setValue("src", e.target.value)}
+              value={form.watch('src')}
+              onChange={(e) => form.setValue('src', e.target.value)}
               label="Video Source"
               placeholder="https://example.com/video.mp4"
             />
             <Input
               labelPlacement="outside"
-              value={form.watch("poster")}
-              onChange={(e) => form.setValue("poster", e.target.value)}
+              value={form.watch('poster')}
+              onChange={(e) => form.setValue('poster', e.target.value)}
               label="Poster"
               placeholder="https://example.com/poster.jpg"
             />
             <Input
               labelPlacement="outside"
-              value={form.watch("title")}
-              onChange={(e) => form.setValue("title", e.target.value)}
+              value={form.watch('title')}
+              onChange={(e) => form.setValue('title', e.target.value)}
               label="Title"
               placeholder="Video Title"
             />
             <Input
               labelPlacement="outside"
-              value={form.watch("subtitle")}
-              onChange={(e) => form.setValue("subtitle", e.target.value)}
+              value={form.watch('subtitle')}
+              onChange={(e) => form.setValue('subtitle', e.target.value)}
               label="Subtitle"
               placeholder="Video Subtitle"
             />
             <Switch
               classNames={{
-                wrapper: "group-data-[selected=true]:bg-default-900",
-                thumb: "group-data-[selected=true]:bg-default-100",
+                wrapper: 'group-data-[selected=true]:bg-default-900',
+                thumb: 'group-data-[selected=true]:bg-default-100',
               }}
-              isSelected={form.watch("hideSliderThumb") ?? false}
-              onValueChange={(value) => form.setValue("hideSliderThumb", value)}
+              isSelected={form.watch('hideSliderThumb') ?? false}
+              onValueChange={(value) => form.setValue('hideSliderThumb', value)}
             >
               Hide Slider Thumb
             </Switch>
             <Switch
               classNames={{
-                wrapper: "group-data-[selected=true]:bg-default-900",
-                thumb: "group-data-[selected=true]:bg-default-100",
+                wrapper: 'group-data-[selected=true]:bg-default-900',
+                thumb: 'group-data-[selected=true]:bg-default-100',
               }}
-              isSelected={form.watch("autoPlay") ?? false}
-              onValueChange={(value) => form.setValue("autoPlay", value)}
+              isSelected={form.watch('autoPlay') ?? false}
+              onValueChange={(value) => form.setValue('autoPlay', value)}
             >
               Auto Play
             </Switch>
             <Switch
               classNames={{
-                wrapper: "group-data-[selected=true]:bg-default-900",
-                thumb: "group-data-[selected=true]:bg-default-100",
+                wrapper: 'group-data-[selected=true]:bg-default-900',
+                thumb: 'group-data-[selected=true]:bg-default-100',
               }}
-              isSelected={form.watch("loop") ?? false}
-              onValueChange={(value) => form.setValue("loop", value)}
+              isSelected={form.watch('loop') ?? false}
+              onValueChange={(value) => form.setValue('loop', value)}
             >
               Loop
             </Switch>
             <Switch
               classNames={{
-                wrapper: "group-data-[selected=true]:bg-default-900",
-                thumb: "group-data-[selected=true]:bg-default-100",
+                wrapper: 'group-data-[selected=true]:bg-default-900',
+                thumb: 'group-data-[selected=true]:bg-default-100',
               }}
-              isSelected={form.watch("showControls") ?? false}
-              onValueChange={(value) => form.setValue("showControls", value)}
+              isSelected={form.watch('showControls') ?? false}
+              onValueChange={(value) => form.setValue('showControls', value)}
             >
               Show Controls
             </Switch>
